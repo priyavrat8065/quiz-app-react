4e03a5a9a5391a5560741e3feb44a2650a1d9023
Priyavrat Chaudhary <priyavratchaudhary862@gmail.com>
2025-07-26 00:16:43 +0530

    fix bug

--- src/component/SkipButton.js
+++ src/component/SkipButton.js
@@ -1,18 +1,9 @@
 function SkipButton({ dispatch, index, totalQuestions, answer }) {
-  if (index < totalQuestions - 1 && answer === null)
+  if (index <= totalQuestions - 1 && answer === null)
     return (
       <button
         className="btn btn-ui"
         onClick={() => dispatch({ type: "skipQuestion" })}
-      >
-        Skip
-      </button>
-    );
-  if (index === totalQuestions - 1)
-    return (
-      <button
-        className="btn btn-ui"
-        onClick={() => dispatch({ type: "finish" })}
       >
         Skip
       </button>
